c95fcb9a58f1dcf0d23d23d7ae15fa52f59e779d
Ali29Mahmoud <113936293+Ali29Mahmoud@users.noreply.github.com>
2022-12-27 17:23:58 +0200

    colors are done

--- headers/newGame.h
+++ headers/newGame.h
@@ -1,4 +1,9 @@
 #pragma once
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KWHT  "\x1B[37m"
 struct cell {
 	char player;
 	char value;
